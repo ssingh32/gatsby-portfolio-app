06b4322b4be6f64037c31b1a662510f9565abe75
Sukhdip Singh Nijjar <sukhdipsinghnijjar@dhcp-10-11-202-53.wireless.pacific.edu>
2019-06-27 11:49:55 -0700

    adding a header footer and layout component to render on each page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,17 @@
-import React from "react"
+import React from 'react';
+import { Link } from 'gatsby';
+import Layout from '../components/layout';
 
-export default () => <div>Hello world!</div>
+const Index = () => {
+    return (
+        <Layout>
+        <h1>Home Page</h1>
+        <Link to="/about">About Page</Link>
+        <Link to="/projects">Projects Page</Link>
+        <Link to="/experience">Experience Page</Link>
+        <Link to="/contact">Contact Page</Link>
+        </Layout>
+    )
+}
+
+export default Index;
