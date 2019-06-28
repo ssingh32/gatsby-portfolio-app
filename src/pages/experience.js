dbc1d429e454d38a739c19394da58b23e90f077a
Sukhdip Singh Nijjar <sukhdipsinghnijjar@dhcp-10-11-202-53.wireless.pacific.edu>
2019-06-28 14:27:18 -0700

    pulled in all prcompanies into experience page

--- src/pages/experience.js
+++ src/pages/experience.js
@@ -1,12 +1,45 @@
 import React from 'react';
 import Layout from '../components/layout';
 import Head from '../components/head';
+import { Link, graphql, useStaticQuery } from 'gatsby';
+import projectStyles from './projects.module.scss';
 
 const Experience = () => {
+    const postData = useStaticQuery(graphql` query {
+        allContentfulWorkExperience(
+            sort: {
+              fields:endDate,
+              order:DESC
+            }
+          ) { edges {
+                node {
+                    title
+                    slug
+                    companyName
+                    startDate(formatString: "MMMM Do, YYYY")
+                    endDate(formatString: "MMMM Do, YYYY")
+                }
+            }
+        }
+      }`);
+
+
     return (
         <Layout>
         <Head title="Experience"/>
-        <h1>Experience Page</h1>
+        <h1>Work Experience</h1>
+        <ol className={projectStyles.posts}>
+            {postData.allContentfulWorkExperience.edges.map((edge) => {
+                return (
+                    <li className={projectStyles.post} key={edge.node.title}>
+                      <Link to={`/experience/${edge.node.slug}`}>
+                          <h2>{edge.node.companyName}, {edge.node.title}</h2>
+                          <p>{edge.node.startDate} - {edge.node.endDate ? edge.node.endDate : "Present"}</p>
+                      </Link>
+                    </li>
+                )
+            })}
+        </ol>
         </Layout>
     )
 }
