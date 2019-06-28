30e27aefd82dcfbbd1102c63b64ddf6658ea7016
Sukhdip Singh Nijjar <sukhdipsinghnijjar@dhcp-10-11-202-53.wireless.pacific.edu>
2019-06-28 13:20:38 -0700

    pulled in projects for template and added a 404 page

--- src/templates/project.js
+++ src/templates/project.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import Layout from '../components/layout';
 import { graphql } from 'gatsby';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import Head from '../components/head';
+export const query = graphql`query($slug: String!) {
+    contentfulProjects(slug: {eq: $slug}) {
+          title
+          slug
+          startDate(formatString: "MMMM Do, YYYY")
+          body {
+            json
+          }
+        }
+  }`;
 
-const Project = () => {
+const Project = (props) => {
+    // const options = {
+    //     renderNode: {
+    //       "embedded-asset-block": (node) => {
+    //         const alt = node.data.target.fields.title['en-US'];
+    //         const url= node.data.target.fields.file['en-US'].url;
+    //         return <img src={url} alt={alt}/>
+    //       }
+    //     }
+    //   }
+
     return (
         <Layout>
-        Project Template Page
+            <Head title={props.data.contentfulProjects.title}/>
+            <h1>{props.data.contentfulProjects.title}</h1>
+            <p>{props.data.contentfulProjects.startDate}</p>
+            {documentToReactComponents(props.data.contentfulProjects.body.json)}
         </Layout>
     )
 }
