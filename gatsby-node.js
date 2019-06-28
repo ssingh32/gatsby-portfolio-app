e83dda16d808a96da7bc2c7ebfc113b3245d903a
Sukhdip Singh Nijjar <sukhdipsinghnijjar@dhcp-10-11-202-53.wireless.pacific.edu>
2019-06-28 13:40:31 -0700

    added a template page for experience and dynamically created a page for all

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports.createPages = async ({graphql, actions}) => {
     const { createPage } = actions;
     const projectTemplate = path.resolve('./src/templates/project.js');
+    const experienceTemplate = path.resolve('./src/templates/experience.js');
 
     const res = await graphql(`query {
       allContentfulProjects {
@@ -14,13 +15,33 @@ module.exports.createPages = async ({graphql, actions}) => {
       }
     }`);
 
-      res.data.allContentfulProjects.edges.forEach((edge) => {
-            createPage({
-                component: projectTemplate,
-                path: `/projects/${edge.node.slug}`,
-                context: {
-                    slug : edge.node.slug
-                }
-            })
+    const experienceRes = await graphql(`query {
+      allContentfulWorkExperience {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
+    }`);
+
+    res.data.allContentfulProjects.edges.forEach((edge) => {
+          createPage({
+              component: projectTemplate,
+              path: `/projects/${edge.node.slug}`,
+              context: {
+                  slug : edge.node.slug
+              }
+          })
+    })
+
+    experienceRes.data.allContentfulWorkExperience.edges.forEach((edge) => {
+      createPage({
+          component: experienceTemplate,
+          path: `/experience/${edge.node.slug}`,
+          context: {
+              slug : edge.node.slug
+          }
       })
+  })
 }
---
+++ src/templates/experience.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Layout from '../components/layout';
+import { graphql } from 'gatsby';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import Head from '../components/head';
+
+const Experience = (props) => {
+
+
+    return (
+        <Layout>
+            Experience Page Layout
+        </Layout>
+    )
+}
+
+export default Experience;
